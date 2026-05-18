c691d4643f5ce11f697aee6fd3c18835af45ed21
Acacio Santos <b6santos81@gmail.com>
2026-05-18 04:42:19 +0100

    Create premium transactions interface

--- frontend/financial-app/src/app/pages/transactions/transactions.ts
+++ frontend/financial-app/src/app/pages/transactions/transactions.ts
@@ -1,10 +1,52 @@
 import { Component } from '@angular/core';
 
+import { CommonModule } from '@angular/common';
+
+import { SidebarComponent }
+from '../../components/sidebar/sidebar';
 
 @Component({
   selector: 'app-transactions',
-  imports: [],
+
+  standalone: true,
+
+  imports: [
+    CommonModule,
+    SidebarComponent
+  ],
+
   templateUrl: './transactions.html',
-  styleUrl: './transactions.css',
+
+  styleUrl: './transactions.css'
 })
-export class TransactionsComponent {}
+
+export class TransactionsComponent {
+
+  showModal = false;
+
+  transactions = [
+
+    {
+      title: 'Salary',
+      category: 'Income',
+      type: 'Income',
+      amount: 320000
+    },
+
+    {
+      title: 'Internet',
+      category: 'Bills',
+      type: 'Expense',
+      amount: 15000
+    },
+
+    {
+      title: 'Food',
+      category: 'Shopping',
+      type: 'Expense',
+      amount: 40000
+    }
+
+  ];
+
+}
