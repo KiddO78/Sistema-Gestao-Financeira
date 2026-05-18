8f155e2db30f4323f1c46b88beb811bc7e32a654
Acacio Santos <b6santos81@gmail.com>
2026-05-18 04:17:27 +0100

    Add financial chart and recent transactions section

--- frontend/financial-app/src/app/pages/dashboard/dashboard.ts
+++ frontend/financial-app/src/app/pages/dashboard/dashboard.ts
@@ -1,13 +1,141 @@
-import { Component } from '@angular/core';
-import { SidebarComponent } from '../../components/sidebar/sidebar';
+import {
+  Component,
+  AfterViewInit
+} from '@angular/core';
+
+import { SidebarComponent }
+from '../../components/sidebar/sidebar';
+
+import {
+  Chart,
+  registerables
+} from 'chart.js';
+
+Chart.register(...registerables);
+
 @Component({
   selector: 'app-dashboard',
   standalone: true,
   imports: [SidebarComponent],
-
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css'
 })
-export class DashboardComponent {
+
+export class DashboardComponent
+implements AfterViewInit {
+
+  ngAfterViewInit(): void {
+
+    new Chart('financeChart', {
+
+      type: 'line',
+
+      data: {
+
+        labels: [
+          'Jan',
+          'Feb',
+          'Mar',
+          'Apr',
+          'May',
+          'Jun'
+        ],
+
+        datasets: [
+
+          {
+            label: 'Income',
+
+            data: [
+              120,
+              190,
+              300,
+              250,
+              420,
+              500
+            ],
+
+            borderColor: '#10b981',
+
+            backgroundColor:
+              'rgba(16,185,129,0.2)',
+
+            tension: 0.4,
+
+            fill: true
+          },
+
+          {
+            label: 'Expenses',
+
+            data: [
+              80,
+              100,
+              180,
+              170,
+              210,
+              260
+            ],
+
+            borderColor: '#ef4444',
+
+            backgroundColor:
+              'rgba(239,68,68,0.2)',
+
+            tension: 0.4,
+
+            fill: true
+          }
+
+        ]
+      },
+
+      options: {
+
+        responsive: true,
+
+        plugins: {
+
+          legend: {
+
+            labels: {
+
+              color: 'white'
+
+            }
+
+          }
+
+        },
+
+        scales: {
+
+          x: {
+
+            ticks: {
+
+              color: '#cbd5e1'
+
+            }
+
+          },
+
+          y: {
+
+            ticks: {
+
+              color: '#cbd5e1'
+
+            }
+
+          }
+
+        }
+
+      }
+
+    });
+
+  }
 
 }
